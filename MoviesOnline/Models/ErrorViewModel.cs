9537d482576df7b32a5de3c607e0247bc7a32184
VUYANI NGCOBO <109946638+DXgriz@users.noreply.github.com>
2023-11-08 18:10:54 +0200

    Add project files.

---
+++ MoviesOnline/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MoviesOnline.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
